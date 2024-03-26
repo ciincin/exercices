2116572aaf1dcd88b24e761a6d28e2008ec88649
Cinthya <cinthyaredondo12@gmail.com>
2024-03-26 11:18:19 +0100

    I solved 5-ej

--- javascript/5-ej/exercise.js
+++ javascript/5-ej/exercise.js
@@ -8,5 +8,11 @@ const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
 
+person2.firstName = "Simon";
+
 console.log(person1);
 console.log(person2);
+
+//Cuando se asigna un objeto a otra variable no se crea una copia
+// si no una referencia, por lo que cuando modificas una propiedad
+// en person2, como está apuntando a person1, también se modifica en person1
