f02f6bf79f54c047251460f56c3cfcfe30837820
Cinthya <cinthyaredondo12@gmail.com>
2024-04-22 16:30:17 +0200

    I solved request-1 ex

--- javascript/fetch/todo-list/exercise.js
+++
@@ -1,8 +0,0 @@
-async function recoverToDos() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos");
-  // console.log(data)
-  const toDos = await data.json();
-  console.log(typeof toDos);
-}
-
-recoverToDos();
