e8d7a18da6feebc261817a334570bd647929ac5d
Cinthya <cinthyaredondo12@gmail.com>
2024-04-22 11:20:35 +0200

    fetch exercises did in the class

--- javascript/fetch/Network-Requests-1/exercise.js
+++ javascript/fetch/Network-Requests-1/exercise.js
@@ -1,8 +1,7 @@
 async function recoverToDos() {
   const data = await fetch("https://jsonplaceholder.typicode.com/todos");
-  // console.log(data)
   const toDos = await data.json();
-  console.log(typeof toDos);
+  console.log(toDos);
 }
 
 recoverToDos();
