86588ea6599ee706fa95193f6ff7d257c82aa1af
Cinthya <cinthyaredondo12@gmail.com>
2024-05-27 17:51:26 +0200

    I solve a new exercise

---
+++ uncontrolledForms-1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UncontrolledLogin from './UncontrolledLogin'
+
+function App() {
+  
+
+  return (
+    <>
+      <UncontrolledLogin/>
+    </>
+  )
+}
+
+export default App
