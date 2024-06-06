cf9ec44c908e96d12f3fdc0ae3876a1f0934629b
Cinthya <cinthyaredondo12@gmail.com>
2024-06-06 11:07:44 +0200

    I added componentComposition ex

--- componentComposition/src/App.jsx
+++ componentComposition/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Container from './components/container/Container'
+import "./App.css";
+import Container from "./components/container/Container";
 
 function App() {
-
   return (
     <>
       {/* <Container title={"This is the collapsable title"} /> */}
-      <Container title="This is the collapsable title" >
-      <div>This is children</div>
-    
+      <Container title="Click here to see the children">
+        <h2>This is the children</h2>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
