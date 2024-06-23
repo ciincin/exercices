f7a1b6fbcfe7a9213d82da13a2e373c8151fb843
Cinthya <cinthyaredondo12@gmail.com>
2024-06-23 12:40:36 +0200

    I added another ts exercise

---
+++ dist/exercise-2-create-intersection-types-start.js
@@ -0,0 +1,18 @@
+/**
+ * START: Follow the instructions below.
+ */
+const countryA = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"]
+};
+// Use the type alias syntax: type CountryWithStatistics = type;
+//
+// Add a type annotation with the `CountryWithStatistics` type on
+// the variable `countryB`.
+const countryB = {
+    name: "China",
+    code: "CN",
+    population: 1412600000
+};
+export {};
