1611e9a8cf4a304ab9ccae6ab5870fbbdaa5ff47
Cinthya <cinthyaredondo12@gmail.com>
2024-07-03 17:23:45 +0200

    I addes new exercise

--- commandLineArt/index.js
+++ commandLineArt/index.js
@@ -1,10 +1,10 @@
 const figlet = require("figlet");
-figlet("Hello world !", (error, data)=>{
-    if(error){
-        console.log("something went wrong...");
-        console.dir(error);
-        return
-    } else {
-        console.log(data);
-    }
-})
+figlet("Hello world !", (error, data) => {
+  if (error) {
+    console.log("something went wrong...");
+    console.dir(error);
+    return;
+  }
+
+  console.log(data);
+});
