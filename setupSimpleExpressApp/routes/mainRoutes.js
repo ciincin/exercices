dfb47227ab9d5173c8d1b887bae00fc4bb383eb4
Cinthya <cinthyaredondo12@gmail.com>
2024-07-11 10:40:29 +0200

    I added gitignore to my exercises

--- setupSimpleExpressApp/routes/mainRoutes.js
+++ setupSimpleExpressApp/routes/mainRoutes.js
@@ -1,8 +1,6 @@
-
 const express = require("express");
 const router = express.Router();
-const planets = require("../data/planets.js")
-
+let planets = require("../data/planets.js");
 
 router.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -14,14 +12,36 @@ router.get("/planets", (req, res) => {
 });
 
 router.get("/planets/:id", (req, res) => {
-  res.json(planets);
+  const { id } = req.params;
+  const resquestedPlanet = planets.find((planet) => planet.id === Number(id));
+  res.json(resquestedPlanet);
 });
 
-// Error handling middleware (must be placed after all routes)
-router.use((error, req, res, next)=>{
+router.post("/planets", (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  const id = planets.length + 1;
 
-  console.error(error.stack);
-  res.status(500).send("Something is wrong")
+  const newPlanet = { id: id, name: name };
+
+  planets.push(newPlanet);
+  res.status(201).json({ msg: "success!" });
+  // res.status(201).json(planets)
+});
+
+
+router.put("/planets/:id", (req, res) =>{
+  const {id} = req.params;
+  const {name} = req.body;
+  const changePlanet = planets.find(planet => planet.id === Number(id))
+  changePlanet.name = name
+  res.status(200).json({changePlanet, planets})
 })
+
+// Error handling middleware (must be placed after all routes)
+router.use((error, req, res, next) => {
+  console.error(error.stack);
+  res.status(500).send("Something is wrong");
+});
 
 module.exports = router;
