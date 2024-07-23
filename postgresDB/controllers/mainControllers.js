9fff552ee6b37f52fa1b5cad7729e99daf23e62a
Cinthya <cinthyaredondo12@gmail.com>
2024-07-23 12:07:12 +0200

    I finish the exercise

--- postgresDB/controllers/mainControllers.js
+++ postgresDB/controllers/mainControllers.js
@@ -20,40 +20,40 @@ async function setupDB() {
   console.log(planets2);
 }
 
-setupDB();
+// setupDB();
 
 const planetSchema = Joi.object({
-  id: Joi.number(),
-  name: Joi.string(),
+  name: Joi.string().alphanum(),
 });
+
+//Joi.string().alphanum() si usamos esto, no ponemos {name}, solo ponemos name en la validación
 
 const mainControllers = {
   home: (req, res) => {
     res.send("Hello, World!");
   },
-  getPlanets: async(req, res) => {
-    const planetList =await db.many(`SELECT * FROM planets`);
+  getPlanets: async (req, res) => {
+    const planetList = await db.many(`SELECT * FROM planets ORDER BY id`);
     res.status(200).json(planetList);
   },
-  GetPlanetById: async(req, res) => {
+  GetPlanetById: async (req, res) => {
     const { id } = req.params;
 
-
-    const resquestedPlanet = await db.oneOrNone(`SELECT * FROM planets WHERE id = $1`, Number(id))
+    const resquestedPlanet = await db.oneOrNone(
+      `SELECT * FROM planets ORDER BY id WHERE id = $1`,
+      Number(id)
+    );
     res.json(resquestedPlanet);
   },
   error: async (req, res) => {
     throw new Error("Async error");
   },
 
-  createPlanet: (req, res) => {
+  createPlanet: async (req, res) => {
     console.log(req.body);
     const { name } = req.body;
-    const id = planets.length + 1;
 
-    const newPlanet = { id: id, name: name };
-
-    const validation = planetSchema.validate(newPlanet);
+    const validation = planetSchema.validate({ name });
     console.log(validation);
 
     if (validation.error) {
@@ -61,31 +61,38 @@ const mainControllers = {
       return;
     }
 
-    planets = [...planets, newPlanet];
+    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
 
-    res.status(201).json({ planets, msg: "success!" });
-    // res.status(201).json(planets)
+    const planetChart = await db.many(`SELECT * FROM planets ORDER BY id`);
+    res.status(201).json({ planetChart, msg: "success!" });
   },
-  updatePlanet: (req, res) => {
+  updatePlanet: async (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
-    // const changePlanet = planets.find((planet) => planet.id === Number(id));
 
-    planets.map((planet) => {
-      if (planet.id === Number(id)) {
-        planet.name = name;
-      }
-      return planet;
-    });
+    const validation = planetSchema.validate({ name });
+    console.log(validation);
 
-    res.status(200).json({ planets });
+    if (validation.error) {
+      res.status(400).json(validation.error.details[0].message);
+      return;
+    }
+
+    await db.none(`UPDATE planets SET name = $2 WHERE id= $1`, [
+      Number(id),
+      name,
+    ]);
+
+    const planetChart = await db.many(`SSELECT * FROM planets ORDER BY id`);
+    res.status(200).json({ planetChart, msg: "success!" });
   },
-  deletePlanet: (req, res) => {
+  deletePlanet: async (req, res) => {
     const { id } = req.params;
-    const newPlanets = planets.filter((planet) => {
-      return planet.id != Number(id);
-    });
-    res.status(200).json({ msg: "planet delete", newPlanets });
+
+    await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
+
+    const planetChart = await db.many(`SELECT * FROM planets ORDER BY id`);
+    res.status(200).json({ msg: "planet delete", planetChart });
   },
 };
 
