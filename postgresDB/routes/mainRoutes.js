d5dcb569e581c4b4e51016b25863e723ba98bc57
Cinthya <cinthyaredondo12@gmail.com>
2024-07-24 15:21:07 +0200

    I finished the exercise

--- postgresDB/routes/mainRoutes.js
+++ postgresDB/routes/mainRoutes.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, "assets/")
     },
-    fileName: (req, file, cb)=>{
+    filename: (req, file, cb)=>{
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()* 1E9)
         cb(null, file.fieldname + "-" + uniqueSuffix + ".jpg")
     }
